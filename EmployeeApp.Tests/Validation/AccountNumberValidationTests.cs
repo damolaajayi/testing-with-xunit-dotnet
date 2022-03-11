ebfa95459264617f23b1ef3dde62ea1c4d2ee27e
damolaajayi <adedamola.ajayi@titantrustbank.com>
2022-03-11 16:40:02 +0100

    first commit

---
+++ EmployeeApp.Tests/Validation/AccountNumberValidationTests.cs
@@ -0,0 +1,46 @@
+﻿using EmployeesApp.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EmployeeApp.Tests.Validation
+{
+    public class AccountNumberValidationTests
+    {
+        private readonly AccountNumberValidation? _validation;
+        public AccountNumberValidationTests() => _validation = new AccountNumberValidation();
+
+        [Fact]
+        public void IsValid_ValidAccountNumber_ReturnsTrue()
+            => Assert.True(_validation?.IsValid("123-4563234576-23"));
+
+        [Theory]
+        [InlineData("1234-3454565676-23")]
+        [InlineData("12-3454565676-23")]
+        public void IsValid_ValidAccountNumber_ReturnsFalse(string accountNumber)
+            => Assert.False(_validation?.IsValid(accountNumber));
+
+        [Theory]
+        [InlineData("123-345456567-23")]
+        [InlineData("123-345456567633-23")]
+        public void IsValid_AccountNumberMiddlePartWrong_ReturnsFalse(string accNumber)
+    => Assert.False(_validation?.IsValid(accNumber));
+
+
+        [Theory]
+        [InlineData("123-3434545656-2")]
+        [InlineData("123-3454565676-233")]
+        public void IsValid_AccountNumberLastPartWrong_ReturnsFalse(string accNumber)
+            => Assert.False(_validation?.IsValid(accNumber));
+
+        [Theory]
+        [InlineData("123-345456567633=23")]
+        [InlineData("123+345356567633-23")]
+        [InlineData("123+345456567633=23")]
+        public void IsValid_InvalidDelimiters_ThrowsArgumentException(string accNumber)
+            => Assert.Throws<ArgumentException>(() => _validation?.IsValid(accNumber));
+    }
+}
